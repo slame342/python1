eb6264d17478b87bbc085a41c7a8d57631a28ace
slame342 <105666656+slame342@users.noreply.github.com>
2026-04-05 23:07:47 +0500

    new

---
+++ functions.py
@@ -0,0 +1,41 @@
+
+
+def say_hi():
+    print("я купил кота" + "1")
+
+# print("я купил кота1")
+#
+# print("я купил кота2")
+#
+# print("я купил кота3")
+#
+# print("я купил кота4")
+
+say_hi()
+
+
+
+
+
+
+# while var_int1 < 500:
+#     var_int1 = var_int1 + 1
+#     print(var_int1)
+#     if var_int1 == 499:
+#         print('вы дошли до 500')
+#
+#
+#
+# while var_int1 < 500:
+#     var_int1 = var_int1 + 1
+#     print(var_int1)
+#     if var_int1 == 499:
+#         print('вы дошли до 500')
+#
+#
+# while var_int1 < 500:
+#     var_int1 = var_int1 + 1
+#     print(var_int1)
+#     if var_int1 == 499:
+#         print('вы дошли до 500')
+
