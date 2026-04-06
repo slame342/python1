b222c1e4995c6198bfe644fcca8d20e2905471da
slame342 <105666656+slame342@users.noreply.github.com>
2026-04-06 17:03:20 +0500

    Update calc.py

--- calc.py
+++ calc.py
@@ -4,6 +4,24 @@ def calculate(num1, num2, operation):
         return num1 + num2
     if operation == "-":
         return num1 - num2
+    if operation == "*":
+        return num1 * num2
+    if operation == "**":
+        return num1 ** num2
+    # if operation == "%":
+    #     return num1 % num2
+    if operation == "/":
+        if num2 != 0:
+            return num1 / num2
+        else:
+            print("на нуль делить нельзя")
+            return print("введите другое число")
+    if operation == "//":
+        if num2 != 0:
+            return num1 // num2
+        else:
+            print("на нуль делить нельзя")
+            return print("введите другое число")
 
 result = calculate(10, 13, "+")
 print(result)
@@ -11,3 +29,17 @@ print(result)
 result = calculate(5, 13, "-")
 print(result)
 
+result = calculate(5, 13, "*")
+print(result)
+
+result = calculate(13, 2, "**")
+print(result)
+
+# result = calculate(5, 2, "%")
+# print(result)
+
+result = calculate(5, 0, "/")
+print(result)
+
+result = calculate(5, 2, "//")
+print(result)
