62cc75818336709527e6d970674dd45240514090
slame342 <105666656+slame342@users.noreply.github.com>
2026-04-06 16:16:59 +0500

    добавлены операции счета

--- calc.py
+++ calc.py
@@ -1,3 +1,13 @@
 def calculate(num1, num2, operation):
-    print("")
-    
+    # print(num1, num2, operation)
+    if operation == "+":
+        return num1 + num2
+    if operation == "-":
+        return num1 - num2
+
+result = calculate(10, 13, "+")
+print(result)
+
+result = calculate(5, 13, "-")
+print(result)
+
