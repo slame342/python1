cf4d25eeab4b966f402aa88bf7e55829e7905ccf
slame342 <105666656+slame342@users.noreply.github.com>
2026-04-29 22:22:44 +0500

    studied

--- work_with_txt.py
+++ work_with_txt.py
@@ -4,21 +4,29 @@ with open("file.txt") as file: # относительный путь к файл
     # text = file.readline() # читает выбранную строку
     # print(text)
     text = file.readlines() # читает все строки, выдает массив
-    # print(text)
+    print(text)
 
-text = "Pellentasque"
-text1 = text[3::1] # обрезает строку в обычной последовательности от 3 строки и до конца
+    new_list = []
+    for line in text:
+        if len(line) > 1:
+            new_list.append(line[0:-1:1])
+        #     pass
+        #     print(line[0:-1:1])
+    print(new_list)
 
-text = "Pellentasque"
-text1 = text[3:10:1]  # обрезает строку от 3 символа до 10 символа
-
-text = "Pellentasque"
-text1 = text[::-1]  # обрезает строку в обратной последовательности
-
-# print(text1)
-
-city_list = ["Almaty", "Astana", "Taraz", "Shymkent"]
-print(city_list[2:0:-1])
+# text = "Pellentasque"
+# text1 = text[3::1] # обрезает строку в обычной последовательности от 3 строки и до конца
+#
+# text = "Pellentasque"
+# text1 = text[3:10:1]  # обрезает строку от 3 символа до 10 символа
+#
+# text = "Pellentasque"
+# text1 = text[::-1]  # обрезает строку в обратной последовательности
+#
+# # print(text1)
+#
+# city_list = ["Almaty", "Astana", "Taraz", "Shymkent"]
+# print(city_list[2:0:-1])
 
 
     # for line in text:
