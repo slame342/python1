df6596cfe47395011fe63558ad5e311539932000
slame342 <105666656+slame342@users.noreply.github.com>
2026-05-17 22:34:04 +0500

    studied

--- work_with_json.py
+++ work_with_json.py
@@ -1,5 +1,5 @@
 import requests
-
+import json
 
 var_dict1 = dict(Age=24, Name="Ally") # создание словаря
 print(var_dict1)
@@ -32,6 +32,19 @@ var_dict4 = {
 }
 print(type(var_dict4))
 
-url = "https://jsonplaceholder.typicode.com/posts"
+url = "https://jsonplaceholder.typicode.com/posts/1"
 response = requests.get(url)
-print(response.content.decode())
+json_data = response.content.decode()
+print(json_data)
+
+with open("json_data.json", "w") as file:
+    json.dump(json_data, file)
+    # json.dumps()
+
+# with open("src/dist/new_file.json", "r") as file:
+#     json_data1 = json.loads(file.read())
+#     print(json_data1)
+#     json.load()
+#     json.loads()
+
+
