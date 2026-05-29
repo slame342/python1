a0a06e548c6c893aee2c708b664d9e53d2c92585
slame342 <105666656+slame342@users.noreply.github.com>
2026-05-29 20:03:00 +0500

    studied

--- parser2.py
+++ parser2.py
@@ -1,6 +1,14 @@
 import requests
 import json
 
+
+x = 'apples'
+y = 'lemons'
+z = 'In the basket are %s and %s' % (x, y)
+print(z)
+
+
+
 url = "https://jsonplaceholder.typicode.com/posts"
 # url = "https://api.instantwebtools.net/v1/airlines"
 response = requests.get(url)
@@ -8,13 +16,19 @@ content = response.content
 print(type(content))
 json_data = content.decode()
 print(type(json_data))
-print(json_data)
+# print(json_data)
 airlines = json.loads(json_data)
+print(type(airlines))
+print(airlines[0:2])
 # print(type(json.load(json_data)))
 
 
-for airline in airlines:
-    with open(f"temp/data.{airline['id']}.json", 'w') as file:
+for airline in airlines[0:2]:
+    # with open(f"temp/data.{airline['id']}.json", 'w') as file:
+    #     json.dump(airline, file)
+    # with open("temp/data" + airline['id'] + '.json', 'w') as file:
+    #     json.dump(airline, file)
+    with open("temp1/data_%s.json"  % airline['id'], 'w') as file:
         json.dump(airline, file)
 #     # json.dumps()
 
